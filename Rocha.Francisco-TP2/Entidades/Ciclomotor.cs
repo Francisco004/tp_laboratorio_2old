bc67e115d830166ee080c89a54c75e97ee34a9ea
Francisco004 <Fran_25_02@hotmail.com>
2020-10-05 04:13:58 -0300

    TP 2 Taller Francisco Rocha 2A Final

---
+++ Rocha.Francisco-TP2/Entidades/Ciclomotor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Ciclomotor : Vehiculo
+    {
+        #region Constructor
+        public Ciclomotor(EMarca marca, string chasis, ConsoleColor color)
+            : base(chasis, marca, color)
+        {
+
+        }
+        #endregion
+
+        #region Propiedad
+        /// <summary>
+        /// Ciclomotor son 'Chico'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Chico;
+            }
+        }
+        #endregion
+
+        #region Metodo
+        /// <summary>
+        /// Carga un StringBuilder con los datos del vehiculo
+        /// </summary>
+        /// <returns>String con los datos del vehiculo</returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CICLOMOTOR");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("TAMAÑO : {0}", this.Tamanio);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+        #endregion
+    }
+}
